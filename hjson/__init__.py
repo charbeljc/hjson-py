3e59d46bd239f235d670050bd12b3c4f05a40597
Charbel Jacquin <charbeljacquin@gmail.com>
2019-02-11 03:35:56 +0100

    use plain dict for python 3.6+

--- hjson/__init__.py
+++ hjson/__init__.py
@@ -54,24 +54,31 @@ from .scanner import HjsonDecodeError
 from .decoder import HjsonDecoder
 from .encoderH import HjsonEncoder
 from .encoder import JSONEncoder
+
+
 def _import_OrderedDict():
+    import sys
+    if sys.version_info[:2] >= (3, 6):
+        return dict
     import collections
     try:
         return collections.OrderedDict
     except AttributeError:
         from . import ordered_dict
         return ordered_dict.OrderedDict
+
+
 OrderedDict = _import_OrderedDict()
 
 
 _default_decoder = HjsonDecoder(encoding=None, object_hook=None,
-                               object_pairs_hook=OrderedDict)
+                                object_pairs_hook=OrderedDict)
 
 
 def load(fp, encoding=None, cls=None, object_hook=None, parse_float=None,
-        parse_int=None, object_pairs_hook=OrderedDict,
-        use_decimal=False, namedtuple_as_object=True, tuple_as_array=True,
-        **kw):
+         parse_int=None, object_pairs_hook=OrderedDict,
+         use_decimal=False, namedtuple_as_object=True, tuple_as_array=True,
+         **kw):
     """Deserialize ``fp`` (a ``.read()``-supporting file-like object containing
     a JSON document) to a Python object.
 
@@ -115,15 +122,15 @@ def load(fp, encoding=None, cls=None, object_hook=None, parse_float=None,
 
     """
     return loads(fp.read(),
-        encoding=encoding, cls=cls, object_hook=object_hook,
-        parse_float=parse_float, parse_int=parse_int,
-        object_pairs_hook=object_pairs_hook,
-        use_decimal=use_decimal, **kw)
+                 encoding=encoding, cls=cls, object_hook=object_hook,
+                 parse_float=parse_float, parse_int=parse_int,
+                 object_pairs_hook=object_pairs_hook,
+                 use_decimal=use_decimal, **kw)
 
 
 def loads(s, encoding=None, cls=None, object_hook=None, parse_float=None,
-        parse_int=None, object_pairs_hook=None,
-        use_decimal=False, **kw):
+          parse_int=None, object_pairs_hook=None,
+          use_decimal=False, **kw):
     """Deserialize ``s`` (a ``str`` or ``unicode`` instance containing a JSON
     document) to a Python object.
 
@@ -204,6 +211,7 @@ _default_hjson_encoder = HjsonEncoder(
     int_as_string_bitcount=None,
 )
 
+
 def dump(obj, fp, skipkeys=False, ensure_ascii=True, check_circular=True,
          cls=None, indent=None,
          encoding='utf-8', default=None, use_decimal=True,
@@ -276,31 +284,31 @@ def dump(obj, fp, skipkeys=False, ensure_ascii=True, check_circular=True,
     """
     # cached encoder
     if (not skipkeys and ensure_ascii and
-        check_circular and
-        cls is None and indent is None and
-        encoding == 'utf-8' and default is None and use_decimal
-        and namedtuple_as_object and tuple_as_array
-        and not bigint_as_string and not sort_keys
-        and not item_sort_key and not for_json
-        and int_as_string_bitcount is None
-        and not kw
-    ):
+            check_circular and
+            cls is None and indent is None and
+            encoding == 'utf-8' and default is None and use_decimal
+            and namedtuple_as_object and tuple_as_array
+            and not bigint_as_string and not sort_keys
+            and not item_sort_key and not for_json
+            and int_as_string_bitcount is None
+            and not kw
+            ):
         iterable = _default_hjson_encoder.iterencode(obj)
     else:
         if cls is None:
             cls = HjsonEncoder
         iterable = cls(skipkeys=skipkeys, ensure_ascii=ensure_ascii,
-            check_circular=check_circular, indent=indent,
-            encoding=encoding,
-            default=default, use_decimal=use_decimal,
-            namedtuple_as_object=namedtuple_as_object,
-            tuple_as_array=tuple_as_array,
-            bigint_as_string=bigint_as_string,
-            sort_keys=sort_keys,
-            item_sort_key=item_sort_key,
-            for_json=for_json,
-            int_as_string_bitcount=int_as_string_bitcount,
-            **kw).iterencode(obj)
+                       check_circular=check_circular, indent=indent,
+                       encoding=encoding,
+                       default=default, use_decimal=use_decimal,
+                       namedtuple_as_object=namedtuple_as_object,
+                       tuple_as_array=tuple_as_array,
+                       bigint_as_string=bigint_as_string,
+                       sort_keys=sort_keys,
+                       item_sort_key=item_sort_key,
+                       for_json=for_json,
+                       int_as_string_bitcount=int_as_string_bitcount,
+                       **kw).iterencode(obj)
     # could accelerate with writelines in some versions of Python, at
     # a debuggability cost
     for chunk in iterable:
@@ -402,7 +410,6 @@ def dumps(obj, skipkeys=False, ensure_ascii=True, check_circular=True,
         **kw).encode(obj)
 
 
-
 _default_json_encoder = JSONEncoder(
     skipkeys=False,
     ensure_ascii=True,
@@ -420,12 +427,13 @@ _default_json_encoder = JSONEncoder(
     int_as_string_bitcount=None,
 )
 
+
 def dumpJSON(obj, fp, skipkeys=False, ensure_ascii=True, check_circular=True,
-         cls=None, indent=None, separators=None,
-         encoding='utf-8', default=None, use_decimal=True,
-         namedtuple_as_object=True, tuple_as_array=True,
-         bigint_as_string=False, sort_keys=False, item_sort_key=None,
-         for_json=False, int_as_string_bitcount=None, **kw):
+             cls=None, indent=None, separators=None,
+             encoding='utf-8', default=None, use_decimal=True,
+             namedtuple_as_object=True, tuple_as_array=True,
+             bigint_as_string=False, sort_keys=False, item_sort_key=None,
+             for_json=False, int_as_string_bitcount=None, **kw):
     """Serialize ``obj`` as a JSON formatted stream to ``fp`` (a
     ``.write()``-supporting file-like object).
 
@@ -500,31 +508,31 @@ def dumpJSON(obj, fp, skipkeys=False, ensure_ascii=True, check_circular=True,
     """
     # cached encoder
     if (not skipkeys and ensure_ascii and
-        check_circular and
-        cls is None and indent is None and separators is None and
-        encoding == 'utf-8' and default is None and use_decimal
-        and namedtuple_as_object and tuple_as_array
-        and not bigint_as_string and not sort_keys
-        and not item_sort_key and not for_json
-        and int_as_string_bitcount is None
-        and not kw
-    ):
+            check_circular and
+            cls is None and indent is None and separators is None and
+            encoding == 'utf-8' and default is None and use_decimal
+            and namedtuple_as_object and tuple_as_array
+            and not bigint_as_string and not sort_keys
+            and not item_sort_key and not for_json
+            and int_as_string_bitcount is None
+            and not kw
+            ):
         iterable = _default_json_encoder.iterencode(obj)
     else:
         if cls is None:
             cls = JSONEncoder
         iterable = cls(skipkeys=skipkeys, ensure_ascii=ensure_ascii,
-            check_circular=check_circular, indent=indent,
-            separators=separators, encoding=encoding,
-            default=default, use_decimal=use_decimal,
-            namedtuple_as_object=namedtuple_as_object,
-            tuple_as_array=tuple_as_array,
-            bigint_as_string=bigint_as_string,
-            sort_keys=sort_keys,
-            item_sort_key=item_sort_key,
-            for_json=for_json,
-            int_as_string_bitcount=int_as_string_bitcount,
-            **kw).iterencode(obj)
+                       check_circular=check_circular, indent=indent,
+                       separators=separators, encoding=encoding,
+                       default=default, use_decimal=use_decimal,
+                       namedtuple_as_object=namedtuple_as_object,
+                       tuple_as_array=tuple_as_array,
+                       bigint_as_string=bigint_as_string,
+                       sort_keys=sort_keys,
+                       item_sort_key=item_sort_key,
+                       for_json=for_json,
+                       int_as_string_bitcount=int_as_string_bitcount,
+                       **kw).iterencode(obj)
     # could accelerate with writelines in some versions of Python, at
     # a debuggability cost
     for chunk in iterable:
@@ -532,11 +540,11 @@ def dumpJSON(obj, fp, skipkeys=False, ensure_ascii=True, check_circular=True,
 
 
 def dumpsJSON(obj, skipkeys=False, ensure_ascii=True, check_circular=True,
-          cls=None, indent=None, separators=None,
-          encoding='utf-8', default=None, use_decimal=True,
-          namedtuple_as_object=True, tuple_as_array=True,
-          bigint_as_string=False, sort_keys=False, item_sort_key=None,
-          for_json=False, int_as_string_bitcount=None, **kw):
+              cls=None, indent=None, separators=None,
+              encoding='utf-8', default=None, use_decimal=True,
+              namedtuple_as_object=True, tuple_as_array=True,
+              bigint_as_string=False, sort_keys=False, item_sort_key=None,
+              for_json=False, int_as_string_bitcount=None, **kw):
     """Serialize ``obj`` to a JSON formatted ``str``.
 
     If ``skipkeys`` is false then ``dict`` keys that are not basic types
@@ -620,19 +628,21 @@ def dumpsJSON(obj, skipkeys=False, ensure_ascii=True, check_circular=True,
     if cls is None:
         cls = JSONEncoder
     return cls(
-        skipkeys=skipkeys, ensure_ascii=ensure_ascii,
-        check_circular=check_circular, indent=indent,
-        separators=separators, encoding=encoding, default=default,
-        use_decimal=use_decimal,
-        namedtuple_as_object=namedtuple_as_object,
-        tuple_as_array=tuple_as_array,
-        bigint_as_string=bigint_as_string,
+        skipkeys=skipkeys,
+        ensure_ascii=ensure_ascii,
+        check_circular=check_circular,
+        indent=indent,
+        separators=separators,
+        default=default,
+        # 3.7 use_decimal=use_decimal,
+        # namedtuple_as_object=namedtuple_as_object,
+        # tuple_as_array=tuple_as_array,
+        # bigint_as_string=bigint_as_string,
         sort_keys=sort_keys,
-        item_sort_key=item_sort_key,
-        for_json=for_json,
-        int_as_string_bitcount=int_as_string_bitcount,
+        # item_sort_key=item_sort_key,
+        # for_json=for_json,
+        # int_as_string_bitcount=int_as_string_bitcount,
         **kw).encode(obj)
-
 
 
 def simple_first(kv):
